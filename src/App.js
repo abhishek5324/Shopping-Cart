abb539916cc7164341b5ddaeb1516569dbdd744c
Abhishek Bansal <abhishek2050.cse18@chitkara.edu.in>
2021-05-09 12:00:05 +0530

    buttons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import CartItem from "./CartItem";
 
 function App() {
   return (
     <div>
       <h1> Cart </h1>
+      <CartItem />
     </div>
   );
 }
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,21 +1,47 @@
 import React from 'react';
+// import './index.css';
 
 class CartItem extends React.Component{
+
+    constructor()
+    {
+        super();
+        this.state = {
+            price: 999,
+            title: "Mobile Phone",
+            qty: 1,
+            img: ''
+        }
+    }
     render(){
+
+        const {price,title,qty} = this.state;
         return(
             <div className='cart-item'>
                 <div className='left-block'>
-                    <img />
+                    <img style={styles.image} />
                 </div>
                 <div className='right-block'>
-                    <div>Phone</div>
-                    <div>Rs 999</div>
-                    <div>Qty: 1</div>
+                    <div style={{fontSize:25}}>{title}</div>
+                    <div style={{color: 'grey'}}>{price}</div>
+                    <div style={{color: 'grey'}}>Qty: {qty}</div>
                     <div className='cart-item-actions'>
-                        {/* buttons */}
+                        <img src="https://www.flaticon.com/svg/vstatic/svg/1237/1237946.svg?token=exp=1620541561~hmac=aa7221343631a2e55e50ae1dd40b4943" alt="increase" className="action-icons" />
+                        <img src="https://www.flaticon.com/svg/vstatic/svg/56/56889.svg?token=exp=1620541647~hmac=6e2990ea6a73f11af96c97584b370432" alt="decrease" className="action-icons" />
+                        <img src="https://www.flaticon.com/svg/vstatic/svg/3096/3096673.svg?token=exp=1620541699~hmac=8311ae9d9f141ed62ed0fcaa1f9443f1" alt="delete" className="action-icons" />
                     </div>
                 </div>
             </div>
         )
     }
 }
+
+const styles = {
+    image:{
+        height: 110,
+        width: 110,
+        borderRadius: 4
+    }
+}
+
+export default CartItem;
